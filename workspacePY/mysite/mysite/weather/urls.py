c26ccf208ff774386aee15e291f56dbd97b57fd1
nataraja <riccardo2468@gmail.com>
2017-04-12 00:25:31 +0200

    Various fix in frontend

--- workspacePY/mysite/mysite/weather/urls.py
+++ workspacePY/mysite/mysite/weather/urls.py
@@ -11,7 +11,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^data/$', views.real_time),
-    url(r'^history/$', views.history, name='history'),
+    #url(r'^history/$', views.history, name='history'),
     url(r'^all_plot/$', views.all_plot),
 
     #url(r'^(?P<question_id>[0-9]+)/$', views.details, name='details'), #by regular expression
