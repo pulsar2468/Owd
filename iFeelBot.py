b48ebb1b7927f551bea6ab30f4f9fe389d7fa932
nataraja <riccardo2468@gmail.com>
2017-04-21 00:07:20 +0200

    Added sendStickers

--- iFeelBot.py
+++ iFeelBot.py
@@ -3,7 +3,7 @@ import telepot
 import re
 
 
-# funzione che viene eseguita ad ogni messaggio ricevuto
+
 
 def Julia_handle(message):
         id_sender, msg, chat_id = message['from']['id'],message['text'],message['chat']['id']
@@ -11,21 +11,59 @@ def Julia_handle(message):
         what, name=msg.split(' ',1)
 
         if re.search('[Tt]emperature|[Tt]emp', what):
-            latest_list = []
 
+            latest_list = []
+            city_list = []
             conn = sqlite3.connect('/home/nataraja/Scrivania/db_weather.sqlite')
             c = conn.cursor()
-            sql = 'SELECT "%s".temp ' \
+            sql1 = 'SELECT "%s".temp ' \
                   'FROM "%s",City WHERE City.name="%s".name AND "%s".rowid = (SELECT MAX(rowid) FROM "%s")' % (
                   name, name, name, name, name)
-            for row in c.execute(sql):
-                latest_list.append(row)
+
+            sql2='SELECT City.name FROM City'
+            for row in c.execute(sql2):
+                city_list.append(row[0])
+
+            if name in city_list:
+
+                for row in c.execute(sql1):
+                    latest_list.append(row[0])
+
+                bot.sendMessage(chat_id,'Last Temp: '+str(latest_list))
+            else:
+                bot.sendMessage(chat_id,'City not found! Please choose between these..\n'+str(city_list))
+
             conn.close()
-            bot.sendMessage(chat_id,str(latest_list[0]))
+        if re.search('[Hh]umidity|[Hh]um', what):
+
+            latest_list = []
+            city_list = []
+            conn = sqlite3.connect('/home/nataraja/Scrivania/db_weather.sqlite')
+            c = conn.cursor()
+            sql1 = 'SELECT "%s".humidity ' \
+                  'FROM "%s",City WHERE City.name="%s".name AND "%s".rowid = (SELECT MAX(rowid) FROM "%s")' % (
+                  name, name, name, name, name)
+
+            sql2='SELECT City.name FROM City'
+            for row in c.execute(sql2):
+                city_list.append(row[0])
+
+            if name in city_list:
+
+                for row in c.execute(sql1):
+                    latest_list.append(row[0])
+
+                bot.sendMessage(chat_id,'Last humidity: '+str(latest_list))
+            else:
+                #bot.sendMessage(chat_id,'City not found! Please  choose between these..\n'+str(city_list))
+                bot.sendSticker(chat_id)
+
+            conn.close()
 
 
 
 bot = telepot.Bot('345541407:AAHs9hnV1T7f3Nq1f4qVoK6IpwjkyxYG3GM')
 bot.message_loop(Julia_handle,run_forever=True)
+stickers = ['']
 #while 1: it's equivalent to run_forever=True
     #time.sleep(10)
